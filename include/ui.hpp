29a06b69686294c8022d457c2c2430c7dd9779e9
Nestor Fabian Villar <nesfvillar@gmail.com>
2024-07-19 21:47:11 -0300

    Added noexcept attributes

--- include/ui.hpp
+++ include/ui.hpp
@@ -5,11 +5,11 @@ namespace chip_8
 {
     struct IUserInterface
     {
-        virtual ~IUserInterface() = default;
+        virtual ~IUserInterface() noexcept = default;
 
-        void virtual clear_screen() = 0;
+        void virtual clear_screen() noexcept = 0;
 
-        void virtual draw(size_t x, size_t y, unsigned height) = 0;
+        void virtual draw(size_t x, size_t y, unsigned height) noexcept = 0;
     };
 
     class CommandLineInterface : public IUserInterface
