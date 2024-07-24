9af57adddc04461d11b357bdb47c2f93bdf7e7dc
Nestor Fabian Villar <nesfvillar@gmail.com>
2024-07-24 02:14:16 -0300

    Added opcode class for easy access to nibbles

---
+++ include/opcode.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <cstdint>
+
+
+namespace chip_8
+{
+    class Opcode
+    {
+    public:
+        constexpr Opcode(uint16_t value) noexcept : _value(value) {}
+
+        constexpr Opcode(uint8_t high_byte, uint8_t low_byte) noexcept :
+            _value((high_byte << 8) | low_byte) {}
+
+        uint8_t constexpr a() const noexcept { return (_value & 0xF000) >> 12; }
+
+        uint8_t constexpr x() const noexcept { return (_value & 0x0F00) >> 8; }
+
+        uint8_t constexpr y() const noexcept { return (_value & 0x00F0) >> 4; }
+
+        uint8_t constexpr n() const noexcept { return _value & 0x000F; }
+
+        uint16_t constexpr nnn() const noexcept { return _value & 0x0FFF; }
+
+        uint16_t constexpr nn() const noexcept { return _value & 0x00FF; }
+
+    private:
+        uint16_t const _value;
+    };
+}
