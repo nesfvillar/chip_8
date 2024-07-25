4ac2c20281fae02eec69e263431403ba906b1a73
Nestor Fabian Villar <nesfvillar@gmail.com>
2024-07-24 23:45:26 -0300

    Added function to read binary files lazily

--- src/emulator.cpp
+++ src/emulator.cpp
@@ -1,6 +1,23 @@
-#include "opcode.hpp"
+#include "cpu.hpp"
+#include "instruction.hpp"
+#include "ui.hpp"
+
+#include <filesystem>
+#include <fstream>
+#include <ranges>
+
 
 using namespace chip_8;
+
+auto read_binary(std::filesystem::path const& path)
+{
+    std::ifstream x{ path, std::ios::binary };
+    std::istreambuf_iterator<char> it{ x }, end;
+
+    return std::views::iota(it)
+        | std::views::take_while([end](auto&& i) { return i != end;})
+        | std::views::transform([](auto&& e) { return *e; });
+}
 
 int main(int, char **)
 {
