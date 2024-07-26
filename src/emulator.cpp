d6eab9ed001b8d716e50971106b67673c61d7904
Nestor Fabian Villar <nesfvillar@gmail.com>
2024-07-26 00:28:59 -0300

    Using a view of the file contents is problematic

--- src/emulator.cpp
+++ src/emulator.cpp
@@ -14,9 +14,11 @@ auto read_binary(std::filesystem::path const& path)
     std::ifstream ifstream{ path, std::ios::binary };
     std::istreambuf_iterator<char> it{ ifstream }, end;
 
-    return std::views::iota(it)
-        | std::views::take_while([end](auto&& i) { return i != end;})
-        | std::views::transform([](auto&& i) { return *i; });
+    return std::vector<uint8_t>(it, end);
+
+    // return std::views::iota(it)
+    // | std::views::take_while([end](auto&& i) { return i != end;})
+    // | std::views::transform([](auto&& i) { return *i; });
 }
 
 int main(int, char **)
