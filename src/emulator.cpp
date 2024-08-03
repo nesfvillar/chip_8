28795e5d0a787017024a87f5db45544530f5c45a
Nestor Fabian Villar <nesfvillar@gmail.com>
2024-08-03 00:05:07 -0300

    Added main loop

--- src/emulator.cpp
+++ src/emulator.cpp
@@ -4,10 +4,12 @@
 
 #include <filesystem>
 #include <fstream>
+#include <raylib.h>
 #include <vector>
 
 
 using namespace chip_8;
+
 
 auto read_binary(std::filesystem::path const& path)
 {
@@ -21,6 +23,28 @@ auto read_binary(std::filesystem::path const& path)
     // | std::views::transform([](auto&& i) { return *i; });
 }
 
-int main(int, char **)
+int main(int argc, char** argv)
 {
+    if (argc != 2)
+    {
+        std::exit(1);
+    }
+
+    auto program = read_binary(argv[1]);
+
+    InitWindow(800, 600, "Chip-8 Emulator");
+    SetTargetFPS(60);
+
+    RaylibUserInterface ui{ };
+    Cpu cpu{ program, ui };
+
+    while (!WindowShouldClose())
+    {
+        ui.render();
+
+        if (auto next = cpu.next_operation())
+            cpu.execute_operation(*next);
+        else
+            CloseWindow();
+    }
 }
