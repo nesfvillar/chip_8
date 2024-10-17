a648cfc5bd2bfcfe7af4d68921643043b122607e
Nestor Fabian Villar <nesfvillar@gmail.com>
2024-10-17 20:14:05 -0300

    Added emulator class

---
+++ src/emulator.cpp
@@ -0,0 +1,12 @@
+#include "emulator.hpp"
+
+#include <fstream>
+
+using namespace chip_8;
+
+std::vector<uint8_t> read_binary(std::filesystem::path const &path) {
+  std::ifstream ifstream{path, std::ios::binary};
+  std::istreambuf_iterator<char> it{ifstream}, end;
+
+  return std::vector<uint8_t>(it, end);
+}
--- src/emulator.hpp
+++ src/emulator.hpp
@@ -4,24 +4,18 @@
 #include "state.hpp"
 
 #include <filesystem>
-#include <fstream>
 #include <vector>
 
 namespace chip_8 {
 
+[[nodiscard]]
+std::vector<uint8_t> read_binary(std::filesystem::path const &path);
+
 class Emulator {
 public:
-  Emulator(std::filesystem::path const &path)
-      : _state(std::views::all(_read_binary(path))) {}
+  constexpr Emulator(std::ranges::view auto program) : _state(program) {}
 
 private:
-  std::vector<uint8_t> _read_binary(std::filesystem::path const &path) {
-    std::ifstream ifstream{path, std::ios::binary};
-    std::istreambuf_iterator<char> it{ifstream}, end;
-
-    return std::vector<uint8_t>(it, end);
-  }
-
   [[nodiscard]]
   Opcode constexpr fetch_opcode(uint16_t location) const {
     return Opcode{_state.cpu.fetch_word(location)};
