8c266d7fc0f85adf6ba0ad89c3a0c2b4dfa24ada
Nestor Fabian Villar <nesfvillar@gmail.com>
2024-10-18 00:55:51 -0300

    Creation of drawing area

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,18 @@ using namespace chip_8;
 
 std::string_view constexpr APP_TITLE = "Chip 8 Emulator";
 std::string_view constexpr APP_ID = "org.nesfvillar.Chip_8";
+size_t constexpr APP_WIDTH = 800;
+size_t constexpr APP_HEIGHT = 600;
 
-static void activate_cb(GtkApplication *app) {
+void activate_cb(GtkApplication *app) {
   GtkWidget *window = gtk_application_window_new(app);
-  GtkWidget *label = gtk_label_new("Hello World");
 
   gtk_window_set_title(GTK_WINDOW(window), APP_TITLE.data());
-  gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
-  gtk_window_set_child(GTK_WINDOW(window), label);
+  gtk_window_set_default_size(GTK_WINDOW(window), APP_WIDTH, APP_HEIGHT);
+
+  GtkWidget *drawing_area = gtk_drawing_area_new();
+  gtk_window_set_child(GTK_WINDOW(window), drawing_area);
+
   gtk_window_present(GTK_WINDOW(window));
 }
 
