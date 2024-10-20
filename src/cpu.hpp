bde441182d3e062da87cb184712b4a165a3d19d1
Nestor Fabian Villar <nesfvillar@gmail.com>
2024-10-20 00:35:46 -0300

    Removed unneeded references to views

--- src/cpu.hpp
+++ src/cpu.hpp
@@ -10,7 +10,7 @@ enum Timer { DELAY, SOUND };
 
 class Cpu {
 public:
-  constexpr Cpu(std::ranges::view auto const &program) {
+  constexpr Cpu(std::ranges::view auto program) {
     std::ranges::copy(program, memory.begin() + _PROGRAM_START);
   }
 
