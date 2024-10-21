06c92d2e2c547a0e23bcb62f13023a57e65cd920
Nestor Fabian Villar <nesfvillar@gmail.com>
2024-10-20 21:00:47 -0300

    Added keyboard control

--- src/main.cpp
+++ src/main.cpp
@@ -46,11 +46,120 @@ gboolean tick_cb(GtkWidget *widget, GdkFrameClock *frame_clock, gpointer data) {
   return G_SOURCE_CONTINUE;
 }
 
+void key_pressed_cb(GtkEventController *controller, guint keyval, guint keycode,
+                    GdkModifierType state, Keyboard *keyboard) {
+  switch (keyval) {
+  case GDK_KEY_1:
+    (*keyboard)[0x1] = true;
+    break;
+  case GDK_KEY_2:
+    (*keyboard)[0x2] = true;
+    break;
+  case GDK_KEY_3:
+    (*keyboard)[0x3] = true;
+    break;
+  case GDK_KEY_4:
+    (*keyboard)[0xC] = true;
+    break;
+  case GDK_KEY_q:
+    (*keyboard)[0x4] = true;
+    break;
+  case GDK_KEY_w:
+    (*keyboard)[0x5] = true;
+    break;
+  case GDK_KEY_e:
+    (*keyboard)[0x6] = true;
+    break;
+  case GDK_KEY_r:
+    (*keyboard)[0xD] = true;
+    break;
+  case GDK_KEY_a:
+    (*keyboard)[0x7] = true;
+    break;
+  case GDK_KEY_s:
+    (*keyboard)[0x8] = true;
+    break;
+  case GDK_KEY_d:
+    (*keyboard)[0x9] = true;
+    break;
+  case GDK_KEY_f:
+    (*keyboard)[0xE] = true;
+    break;
+  case GDK_KEY_z:
+    (*keyboard)[0xA] = true;
+    break;
+  case GDK_KEY_x:
+    (*keyboard)[0x0] = true;
+    break;
+  case GDK_KEY_c:
+    (*keyboard)[0xB] = true;
+    break;
+  case GDK_KEY_v:
+    (*keyboard)[0xF] = true;
+    break;
+  }
+}
+
+void key_released_cb(GtkEventController *controller, guint keyval,
+                     guint keycode, GdkModifierType state, Keyboard *keyboard) {
+  switch (keyval) {
+  case GDK_KEY_1:
+    (*keyboard)[0x1] = false;
+    break;
+  case GDK_KEY_2:
+    (*keyboard)[0x2] = false;
+    break;
+  case GDK_KEY_3:
+    (*keyboard)[0x3] = false;
+    break;
+  case GDK_KEY_4:
+    (*keyboard)[0xC] = false;
+    break;
+  case GDK_KEY_q:
+    (*keyboard)[0x4] = false;
+    break;
+  case GDK_KEY_w:
+    (*keyboard)[0x5] = false;
+    break;
+  case GDK_KEY_e:
+    (*keyboard)[0x6] = false;
+    break;
+  case GDK_KEY_r:
+    (*keyboard)[0xD] = false;
+    break;
+  case GDK_KEY_a:
+    (*keyboard)[0x7] = false;
+    break;
+  case GDK_KEY_s:
+    (*keyboard)[0x8] = false;
+    break;
+  case GDK_KEY_d:
+    (*keyboard)[0x9] = false;
+    break;
+  case GDK_KEY_f:
+    (*keyboard)[0xE] = false;
+    break;
+  case GDK_KEY_z:
+    (*keyboard)[0xA] = false;
+    break;
+  case GDK_KEY_x:
+    (*keyboard)[0x0] = false;
+    break;
+  case GDK_KEY_c:
+    (*keyboard)[0xB] = false;
+    break;
+  case GDK_KEY_v:
+    (*keyboard)[0xF] = false;
+    break;
+  }
+}
+
 void activate_cb(GtkApplication *app, Emulator *emulator) {
   auto window = adw_application_window_new(app);
   auto toolbar = adw_toolbar_view_new();
   auto header = adw_header_bar_new();
   auto title = adw_window_title_new(APP_TITLE.data(), nullptr);
+  auto key_controller = gtk_event_controller_key_new();
   auto drawing_area = gtk_drawing_area_new();
 
   adw_application_window_set_content(ADW_APPLICATION_WINDOW(window), toolbar);
@@ -61,13 +170,27 @@ void activate_cb(GtkApplication *app, Emulator *emulator) {
 
   adw_header_bar_set_title_widget(ADW_HEADER_BAR(header), title);
 
+  g_signal_connect_data(GTK_EVENT_CONTROLLER_KEY(key_controller), "key-pressed",
+                        G_CALLBACK(key_pressed_cb),
+                        const_cast<Keyboard *>(&emulator->state().keyboard),
+                        nullptr, G_CONNECT_DEFAULT);
+  g_signal_connect_data(key_controller, "key-released",
+                        G_CALLBACK(key_released_cb),
+                        const_cast<Keyboard *>(&emulator->state().keyboard),
+                        nullptr, G_CONNECT_DEFAULT);
+
   gtk_drawing_area_set_content_width(GTK_DRAWING_AREA(drawing_area), APP_WIDTH);
   gtk_drawing_area_set_content_height(GTK_DRAWING_AREA(drawing_area),
                                       APP_HEIGHT);
   gtk_drawing_area_set_draw_func(
       GTK_DRAWING_AREA(drawing_area), draw_cb,
       const_cast<Screen *>(&emulator->state().screen), nullptr);
+
   gtk_widget_add_tick_callback(drawing_area, tick_cb, emulator, nullptr);
+
+  gtk_widget_set_focusable(GTK_WIDGET(drawing_area), TRUE);
+  gtk_widget_add_controller(GTK_WIDGET(drawing_area),
+                            GTK_EVENT_CONTROLLER(key_controller));
 
   gtk_window_present(GTK_WINDOW(window));
 }
